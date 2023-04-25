fea5709503c5b780f17b801a13c72dcc2689ede8
Esenaliev Argen <56115080+Argen2002@users.noreply.github.com>
2023-04-25 19:41:00 +0600

    Update txt files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField(max_length=500)
+    created_data = models.DateTimeField(default=timezone.now)
+    published_data = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_data=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
